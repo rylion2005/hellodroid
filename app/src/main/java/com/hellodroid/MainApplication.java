80c3b463fbb2004644cc624b7d1d3129ac4ac4bc
rylion <rylion2005@sina.com>
2018-02-04 01:25:54 +0800

    Initial codes

---
+++ app/src/main/java/com/hellodroid/MainApplication.java
@@ -0,0 +1,66 @@
+package com.hellodroid;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
+
+
+/*
+** ==========================================================================================
+** MainApplication
+**   This is a application class which we can do some global initialization here
+**
+** ==========================================================================================
+*/
+public class MainApplication  extends Application  {
+    private static final String TAG = "MainApplication";
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "onCreate");
+        init();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        Log.v(TAG, "onLowMemory");
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        Log.d(TAG, "onTrimMemory(" + level + ")");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    /*
+    ** ---------------------------------------------------------------------------
+    ** init
+    **   Global initialization
+    **
+    ** @PARAM : None
+    ** @RETURN: None
+    **
+    ** NOTES:
+    **   ......
+    ** ---------------------------------------------------------------------------
+    */
+    private void init(){
+    }
+}
