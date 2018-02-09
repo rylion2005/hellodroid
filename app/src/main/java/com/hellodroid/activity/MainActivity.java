bc93080761f5d9be4d71031bc3fb2ab019b24d8c
yl7 <ryl@unipro.cc>
2018-02-09 16:35:07 +0800

    add lan scanner file

--- app/src/main/java/com/hellodroid/activity/MainActivity.java
+++ app/src/main/java/com/hellodroid/activity/MainActivity.java
@@ -1,22 +1,23 @@
 package com.hellodroid.activity;
 
-import android.os.Handler;
-import android.os.Message;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import com.hellodroid.R;
+import com.hellodroid.lan.Scanner;
 import com.hellodroid.socket.SocketListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTXVContents;
+
     private final SocketListener sl = new SocketListener(this);
     private final InCallback mCallBack = new InCallback();
 
-    private TextView mTXVContents;
+    private Scanner sc = new Scanner();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
         mTXVContents = findViewById(R.id.TXV_TextContents);
     }
+
+/* ============================================================================================== */
+
+    public void scanLan(View v){
+        mTXVContents.append(sc.getMyLocalAddress() + " | " + sc.getNetworkAddress() + "\n");
+    }
+
+
+/* ============================================================================================== */
 
 
     public void becomeAsServer(View v){
@@ -39,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     class InCallback implements SocketListener.IncomingCallBack{
         @Override
         public void onMessageReceived(String text) {
-            mTXVContents.append("\n" + text);
+            //mTXVContents.append("\n" + text);
         }
 
         @Override
